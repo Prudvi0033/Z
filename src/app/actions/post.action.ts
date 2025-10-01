0223a349e5ac612e00fd893f13f125d5b9ec0e35
Prudvi0033 <prudvirajvarma2004@gmail.com>
2025-10-01 16:03:45 +0530

    post actions

---
+++ src/app/actions/post.action.ts
@@ -0,0 +1,150 @@
+"use server";
+
+import { headers } from "next/headers";
+
+import { auth } from "../lib/auth";
+import { prisma } from "../lib/prisma";
+
+export const createPost = async (message: string, postImage?: string) => {
+  const seesion = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!seesion) {
+    return { success: false, error: "Authentication is required" };
+  }
+
+  try {
+    const post = await prisma.post.create({
+      data: {
+        message: message,
+        postImage: postImage,
+        userId: seesion.user.id,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    return { sucess: true, post };
+  } catch (error) {
+    console.error("Error creating post:", error);
+    return { success: false, error: "Failed to create post" };
+  }
+};
+
+export const deletePost = async (postId: string) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    return { success: false, error: "Authentication is required" };
+  }
+
+  try {
+    const userPost = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+    });
+
+    if (!userPost) {
+      return { sucess: false, error: "Post not found" };
+    }
+
+    if (userPost.userId !== session.user.id) {
+      return { success: false, error: "Can't delete this post" };
+    }
+
+    await prisma.post.delete({
+      where: {
+        id: postId,
+      },
+    });
+    return { success: true, message: "Post deleted successfully" };
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    return { success: false, error: "Failed to delete post" };
+  }
+};
+
+export const getUserPosts = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    return { success: false, error: "Authentication is required" };
+  }
+
+  try {
+    const userPosts = await prisma.post.findMany({
+      where: {
+        userId: session.user.id,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+            username: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+            votes: true,
+            bookmarks: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return { sucess: true, userPosts };
+  } catch (error) {
+    console.error("Error fetching user posts:", error);
+    return { success: false, error: "Failed to fetch posts" };
+  }
+};
+
+export const getAllPosts = async () => {
+  try {
+    const allPosts = await prisma.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            image: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+            bookmarks: true,
+            votes: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return { success: true, allPosts };
+  } catch (error) {
+    console.error("Error fetching all posts:", error);
+    return { success: false, error: "Failed to fetch posts" };
+  }
+};
